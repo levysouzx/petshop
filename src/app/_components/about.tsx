4858e36af56bc2995cb6b74c6158b3c002f99f89
Levy <levysousa2006@gmail.com>
2025-02-10 16:52:59 -0300

    novas sections

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import about1img from "../img/about-1.png";
 import about2img from "../img/about-2.png";
+import { Check, MapPin } from "lucide-react";
+import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr";
 export function About() {
   return (
     <section className="bg-[#FDF6EC] py-16 ">
@@ -13,7 +15,7 @@ export function About() {
               fill
               quality={100}
               priority
-              className="object-cover hover: scale-125 transition-all duration-500 "
+              className="object-cover hover:scale-110 transition-all duration-300 "
             />
           </div>
           <div className="absolute w-40 h-40 right-4 -bottom-8 rounded-lg border-4 border-white overflow-hidden ">
@@ -26,8 +28,45 @@ export function About() {
             />
           </div>
         </div>
-        <div className="space-y-6 ">
-          <h2 className="text-4xl font-bold">SOBRE</h2>
+
+        <div className="space-y-6 mt-10">
+          <h2 className="text-4xl font-bold px-5">SOBRE</h2>
+          <p className="text-3xl font-semilbold py- px-5">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos
+            tempore porro praesentium culpa, odio optio fuga veniam accusantium.
+            Commodi vitae enim perferendis molestiae doloremque at iste? Veniam
+            magni laboriosam unde.
+          </p>
+          <ul className="space-y-4">
+            <li className="flex  items-center">
+              <Check className="text-red-500 gap-2" />
+              Aberto desde 2024
+            </li>
+            <li className="flex  items-center">
+              <Check className=" text-red-500 gap-2" />
+              Mais de 10 profissionais em nosso estabelecimento
+            </li>
+            <li className="flex  items-center">
+              <Check className=" text-red-500 gap-2" />
+              Seu pet é nossa prioridade
+            </li>
+          </ul>
+          <div className="flex items-center gap-4">
+            <a
+              href="#"
+              className="bg-[#E84C3D] text-white flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md font-semibold"
+            >
+              <WhatsappLogo className="w-5 h-5 text-white" />
+              Contato via WhatsApp
+            </a>
+            <a
+              href="#"
+              className="bg-bg-[#FDF6EC] text-blac flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md border-black border-2 font-semibold "
+            >
+              <MapPin className="w-5 h-5 text-black" />
+              Nosso endereço
+            </a>
+          </div>
         </div>
       </div>
     </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Hero } from "./_components/hero";
 import { About } from "./_components/about";
+import { Services } from "./_components/services";
+import { Testimonials } from "./_components/testimoials";
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Services />
+      <Testimonials />
     </main>
   );
 }
