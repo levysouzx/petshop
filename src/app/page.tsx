f5203c7de5cf4a8a54f4e409100735d0730a401a
Levy <levysousa2006@gmail.com>
2025-12-12 21:09:47 -0300

    bug: corrigindo erros de builds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { About } from "./_components/about";
 import { Footer } from "./_components/footer";
 import { Hero } from "./_components/hero";
 import { Services } from "./_components/services";
-import { Testimonials } from "./_components/testimonials";
+import { Testimonials } from "./_components/Testimonials";
 
 export default function Home() {
   return (
